77c821319686ee144a2a27ebaec6646015cac13d
Only-zeus <156516190+Only-zeus@users.noreply.github.com>
2026-06-17 18:27:14 +0100

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,14 +1,14 @@
-import { initializeApp } from “https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js”;
-import { getAuth, onAuthStateChanged } from “https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js”;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: “AIzaSyCQfmnqIL4_FlfDVGPl0o_oLdDQK4fUjeo”,
-authDomain: “zella-424f2.firebaseapp.com”,
-projectId: “zella-424f2”,
-storageBucket: “zella-424f2.firebasestorage.app”,
-messagingSenderId: “1008967672362”,
-appId: “1:1008967672362:web:20cdc14e463c1e81949953”,
-databaseURL: “https://zella-424f2-default-rtdb.firebaseio.com”
+apiKey: "AIzaSyCQfmnqIL4_FlfDVGPl0o_oLdDQK4fUjeo",
+authDomain: "zella-424f2.firebaseapp.com",
+projectId: "zella-424f2",
+storageBucket: "zella-424f2.firebasestorage.app",
+messagingSenderId: "1008967672362",
+appId: "1:1008967672362:web:20cdc14e463c1e81949953",
+databaseURL: "https://zella-424f2-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,6 +16,6 @@ const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
 if (!user) {
-window.location.href = “index.html”;
+window.location.href = "index.html”;
 }
 });
